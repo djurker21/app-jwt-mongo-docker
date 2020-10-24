5590f584a6b6a4c77754b5bc223b4cb4544995a3
Yurii Vydai <djurker21@gmail.com>
2020-10-24 10:07:23 +0300

    swagger, user email

--- users/user.model.js
+++ users/user.model.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    email: { type: String, required: true },
     username: { type: String, unique: true, required: true },
     passwordHash: { type: String, required: true },
     role: { type: String, required: true }
--- users/user.service.js
+++ users/user.service.js
@@ -121,6 +121,6 @@ function randomTokenString() {
 }
 
 function basicDetails(user) {
-    const { id, firstName, lastName, username, role } = user;
-    return { id, firstName, lastName, username, role };
+    const { id, email, username, role } = user;
+    return { id, email, username, role };
 }
