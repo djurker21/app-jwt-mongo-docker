e9f72546a08e773ec43eab8ccf34762e44b28001
Yurii Vydai <djurker21@gmail.com>
2020-10-26 10:44:01 +0200

    sending notifications

--- _helpers/notify_users.js
+++ _helpers/notify_users.js
@@ -1,5 +1,9 @@
 module.exports = {notifyUsers};
 
+const models = require('../models');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
+
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 async function notifyUsers() {
@@ -13,7 +17,23 @@ async function notifyUsers() {
     }, theInterval);   
 }
 
-function notify() {
+async function notify() {
     //get notification from sequelize...
+    const notifications = await models.Notification.findAll(
+        {where: {notified: {[Op.ne]: null}}}
+    );
+    if (notifications && notifications.length > 0) {
+        for (const notification of notifications) {
+            const result = await sendEmail(notification);
+            if (result) {
+                await notification.destroy();
+            }   
+        }
+    }
+}
+
+async function sendEmail(notification) {
     console.log('notify users...');
+    console.log(notification);
+    return true;
 }
